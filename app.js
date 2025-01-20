c0663c30c687c8a5ea4c5b160bbd4b176777f974
Elias Rédua <152866528+eliasredua@users.noreply.github.com>
2025-01-20 20:48:43 +0000

    Update app.js

--- app.js
+++ app.js
@@ -48,7 +48,6 @@ function gerarNumeroAleatorio() {
         return gerarNumeroAleatorio();
     } else {
         listaDeNumerosSorteados.push(numeroEscolhido);
-        console.log(listaDeNumerosSorteados);
         return numeroEscolhido;
     }
 }
